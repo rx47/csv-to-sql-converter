3925cf765e5a1c7b31b00bba648d1581d5c9552f
Sheraz Kharal <raysheraz48@gmail.com>
2023-09-04 22:37:30 +0100

    final commit

--- README.md
+++ README.md
@@ -1,21 +1,44 @@
-
 # CSV to SQL Converter
 
-converts csv data to sql code
+This utility provides an efficient way to convert CSV files into SQL scripts. It automates the process of generating SQL `CREATE TABLE` and `INSERT` statements based on the data in your CSV files.
+
+## Features
+
+- **Automatic Table Creation**: Generates SQL statements to create tables named after the CSV files.
+- **Dynamic Data Type Inference**: The utility intelligently infers SQL data types from the CSV data. For instance, columns with all integer values will be inferred as `INT`, and those with string values will be designated as `VARCHAR`.
+- **Null Value Handling**: Recognizes various representations of null values (e.g., "null", "na", "n/a") and converts them to SQL `NULL`.
 
 ## Usage
 
-Simply put the script in the folder with csv files and run it all files in that directory will be converted into sql files
-to use it into you project for converting specific files import the following function from the script `from csv_to_sql_converter import save_data_to_sql_file` and insert the name of the file into the function `save_data_to_sql_file("insert_name_here.csv")`
+1. Place the script in the directory containing your CSV files.
+2. Execute the script. It will process all `.csv` files in the directory.
+3. For each CSV file, an SQL file with the same name but with an `.sql` extension will be created. This SQL file contains the necessary statements to create a table and insert the data from the CSV file.
 
-#### important
-make sure there are no spaces between the file names and the first row of your document
-* file name will act as table name
-* first row of csv file will act as rows names in sql database
+## Example
 
-#### v0.1
-* fixed issues related to `sql format`
+Given a CSV file named `users.csv` with the following content:
 
-#### v0.2
-* minor bugfixes
-* fixed comma issue on the last line replaced it with `;`
+```
+id,name,age
+1,John,25
+2,Alice,28
+3,,30
+```
+
+The script will produce an `users.sql` file with the statements:
+
+```sql
+CREATE TABLE `users` (
+`id` INT,
+`name` VARCHAR(255),
+`age` INT
+);
+
+INSERT INTO `users` (`id`,`name`,`age`)
+VALUES
+(1,'John',25),
+(2,'Alice',28),
+(3,NULL,30);
+```
+
+By using this script, you can quickly transform your CSV datasets into SQL-ready scripts, streamlining the process of integrating data into your databases.
--- csv_to_sql_converter.py
+++ csv_to_sql_converter.py
@@ -1,40 +1,103 @@
 import csv, glob, os
 
-
 def read_csv_file(file_name):
+    """Read a CSV file and return its rows."""
     rows = []
-    with open(file_name, 'r') as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            rows.append(row)
+    try:
+        with open(file_name, 'r') as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                rows.append(row)
+    except FileNotFoundError:
+        print(f"Error: {file_name} not found.")
+        return []
+    except Exception as e:
+        print(f"Error reading {file_name}: {e}")
+        return []
     return rows
 
-def create_sql_insert_statement(file_name):
-    table_name = file_name.split(".")[0]
+def infer_data_type(column_name, data):
+    """Infer SQL data type based on column name and data."""
+    if any(keyword in column_name.lower() for keyword in ["date", "time"]):
+        return "DATE"
+    if any(keyword in column_name.lower() for keyword in ["id", "num"]) and all(item.isdigit() for item in data):
+        return "INT"
+    if any("." in item for item in data):
+        return "DECIMAL(10,2)"
+    return "VARCHAR(255)"
+
+def create_sql_table_statement(file_name):
+    """Generate SQL CREATE TABLE statement based on CSV file."""
+    table_name = os.path.basename(file_name).split(".")[0]
     rows = read_csv_file(file_name)
+
+    if not rows:
+        return
+
+    headers = rows[0]
+    column_definitions = []
+    for i, header in enumerate(headers):
+        column_data = [row[i] for row in rows[1:]]
+        data_type = infer_data_type(header, column_data)
+        column_definitions.append(f"`{header}` {data_type}")
+
+    columns_str = ",\n".join(column_definitions)
+    create_statement = "CREATE TABLE `{}` (\n{}\n);\n".format(table_name, columns_str)
+    return create_statement
+
+def create_sql_insert_statement(file_name):
+    """Create an SQL INSERT statement for a given CSV file."""
+    table_name = os.path.basename(file_name).split(".")[0]
+    rows = read_csv_file(file_name)
+    if len(rows) <= 1:
+        return
     headers = "`,`".join(rows[0])
-    insert_line = "INSERT INTO `" + table_name + "` (`" + headers + "`) VALUES\n"
+
+    # List of values considered as NULL
+    null_values = ["null", "na", "n/a", ""]
+
+    # Adjusting the formation of SQL values
     entries = []
-
     for row in rows[1:]:
-        value = "('" + "', '".join(row) + "'),"
-        entries.append(value)
+        formatted_row = []
+        for item in row:
+            # Check if the item is considered as NULL
+            if item.lower() in null_values:
+                formatted_row.append("NULL")
+            # Check if the item is numeric, including negative numbers and decimals
+            elif item.lstrip('-').replace('.', '', 1).isdigit():
+                formatted_row.append(item)
+            # If it's alphanumeric or has special characters, enclose in quotes
+            else:
+                formatted_row.append(f"'{item}'")
+        entries.append(f"({','.join(formatted_row)})")
 
-    entry_lines = "\n".join(entries)
-    result = insert_line + entry_lines + ";"
 
-    last_comma = result.replace(",;",";")[0:]
-    return last_comma
+    insert_line = f"INSERT INTO `{table_name}` (`{headers}`) VALUES\n"
+    result = insert_line + ",\n".join(entries) + ";"
+    return result
+
+
 
 def save_data_to_sql_file(csv_file_name):
-    output_file_name = csv_file_name.split(".")[0]
-    with open(output_file_name+".sql", "a") as f:
-        print(create_sql_insert_statement(csv_file_name), file=f)
+    """Save the generated SQL statements (CREATE and INSERT) into an .sql file."""
+    create_statement = create_sql_table_statement(csv_file_name)
+    insert_statement = create_sql_insert_statement(csv_file_name)
+
+    if not create_statement or not insert_statement:
+        return
+
+    output_file_name = os.path.basename(csv_file_name).split(".")[0]
+    with open(output_file_name + ".sql", "a") as f:
+        print(create_statement, file=f)
+        print(insert_statement, file=f)
 
 def get_csv_files_from_current_directory():
-    os.getcwd()
+    """Get all CSV files in the current directory and convert them to SQL."""
     for file in glob.glob("*.csv"):
         save_data_to_sql_file(file)
 
 if __name__ == "__main__":
+    print("Starting CSV to SQL conversion...")
     get_csv_files_from_current_directory()
+    print("Conversion completed.")
